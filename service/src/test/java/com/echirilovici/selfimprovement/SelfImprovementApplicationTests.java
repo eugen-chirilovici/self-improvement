ab33c4ff67fcbb9eb98279e6aa58684422cde838
eugenchirilovici <echirilovici123endava@gmail.com>
2023-07-06 20:29:15 +0300

    init

---
+++ service/src/test/java/com/echirilovici/selfimprovement/SelfImprovementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echirilovici.selfimprovement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfImprovementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ service/src/test/java/com/echirilovici/selfimprovement/TestSelfImprovementApplication.java
@@ -0,0 +1,29 @@
+package com.echirilovici.selfimprovement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.RabbitMQContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestSelfImprovementApplication {
+
+	@Bean
+	@ServiceConnection
+	MongoDBContainer mongoDbContainer() {
+		return new MongoDBContainer("mongo:latest");
+	}
+
+	@Bean
+	@ServiceConnection
+	RabbitMQContainer rabbitContainer() {
+		return new RabbitMQContainer("rabbitmq:latest");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(SelfImprovementApplication::main).with(TestSelfImprovementApplication.class).run(args);
+	}
+
+}
